8a0f9e8287f1642f08e8264b8568a29c2c4f96e4
drjonesy <rjermanjones@gmail.com>
2017-07-03 11:10:38 -0700

    change names in main to assist understanding

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main(int argc, char **argv)
 {
 
-	string preamble = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.";
+	string str = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.";
 	
-	vector<string> justWords = splitStringToVector(preamble, " ");
-	vector<WordObj> words = vectorOfWordObjects(justWords);
+	vector<string> strVector = splitStringToVector(str, " ");
+	vector<WordObj> engVector = vectorOfWordObjects(strVector);
 	
-	vector<WordObj> piglatin = engToPiglatin(words); // contains vector of piglatin words
-	string pigString = wordsToString(piglatin); // a string with the words and punctuation in piglatin
-	string pigLow = strToLower(pigString); // applies lowercase to all characters.
-	cout << pigLow << endl;
+	vector<WordObj> piglatinVector = engToPiglatin(engVector); // contains vector of piglatin words
+	string pigString = wordsToString(piglatinVector); // a string with the words and punctuation in piglatin
+	string pigLowercase = strToLower(pigString); // applies lowercase to all characters.
+	cout << pigLowercase << endl;
 
 	return 0;
 }
